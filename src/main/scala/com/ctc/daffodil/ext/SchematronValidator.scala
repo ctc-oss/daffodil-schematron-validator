14b3cadc6434f462807b88094b958f9e5bb6a415
John Wass <wassj@ctc.com>
2020-10-22 10:31:34 -0400

    validation result

--- src/main/scala/com/ctc/daffodil/ext/SchematronValidator.scala
+++ src/main/scala/com/ctc/daffodil/ext/SchematronValidator.scala
@@ -4,18 +4,22 @@ import java.io.InputStream
 
 import com.helger.schematron.svrl.jaxb.{FailedAssert, SchematronOutputType}
 import com.helger.schematron.xslt.SchematronResourceXSLT
-import org.apache.daffodil.api.Validator
-import org.xml.sax.ErrorHandler
+import org.apache.daffodil.api.{ValidationError, ValidationResult, Validator}
 
 import scala.collection.JavaConverters._
 
+case class SchematronValidationError(f: FailedAssert) extends ValidationError {
+  def message: String = f.getText.toString
+  def toThrowable: Throwable = new Exception(f.getText.toString)
+}
+
 class SchematronValidator(sch: SchematronResourceXSLT) extends Validator {
-  def validateXML(document: InputStream, errHandler: ErrorHandler): Unit = {
+  def validateXML(document: InputStream): ValidationResult = {
     val svrl: SchematronOutputType = sch.applySchematronValidationToSVRL(SchUtil.istream(document))
-    svrl.getActivePatternAndFiredRuleAndFailedAssert.asScala.foreach {
-      case f: FailedAssert =>
-        // todo;; return as validation errors
+    val errors = svrl.getActivePatternAndFiredRuleAndFailedAssert.asScala.flatMap {
+      case f: FailedAssert => Some(SchematronValidationError(f))
+      case _               => None
     }
-    SchUtil.printSvrl(svrl)
+    ValidationResult(Seq.empty, errors)
   }
 }
